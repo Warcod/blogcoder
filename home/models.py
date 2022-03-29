e1bb9a5b8c8ce0e1894504bb96d869a7f3c04058
guidoiri <guidoirigoyen@gmail.com>
2022-03-29 00:42:36 -0300

    Creacion de la herencia principal en la app home y modelos iniciales

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
+from dataclasses import Field, field
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    avatar = models.ImageField()
+    
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=40)
+    content = models.TextField()
+    date = models.DateField()
+    image = models.ImageField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    
--- home/views.py
+++ home/views.py
@@ -1,7 +1,28 @@
+from datetime import datetime
+import email
+from email.mime import image
+from unicodedata import name
 from django.shortcuts import render
+from .models import BlogPost, User
+from datetime import date
 
 # Create your views here.
 
 def home(request):
     
-    return render(request, 'home/index.html')
+    user = User(name = 'Guido',
+                last_name = 'Irigoyen',
+                email = 'guidoirigoyen@gmail.com',
+                avatar = ''
+    )
+    user.save()
+    
+    post = BlogPost(title = 'Prueba Post',
+                    content = 'Loremipsuuuum',
+                    date = date.today(),
+                    image = '',
+                    user = user
+    )
+    post.save()
+    
+    return render(request, 'home/home.html', {'title' : post.title , 'content' : post.content, 'date' : post.date})
